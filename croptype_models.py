49d9f80afe003ec48d45212c5b3127e4f8534c00
Depanshu Sani <depanshus@iiitd.ac.in>
2022-07-01 23:29:45 +0530

    croptype_models bug fix

--- croptype_models.py
+++ croptype_models.py
@@ -119,7 +119,7 @@ def get_model(model_name, **kwargs):
                     break
                 state_dict[k] = pretrained_dict[k]
             model.load_state_dict(state_dict)
-    if model_name == 'unet-fc':
+    elif model_name == 'unet-fc':
         num_bands = get_num_bands(kwargs)['all']
         model = make_UNetFC_model(n_class=3, n_channel=num_bands,
                                       timesteps=kwargs.get('num_timesteps'), dropout=kwargs.get('dropout'))
